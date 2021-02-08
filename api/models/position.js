89ea44e523b2c12958b4b56c49a76b5c84d1924c
pdxJaxon <GregoryAJackson@Hotmail.com>
2021-02-08 15:50:37 -0800

    help jesus

--- api/models/position.js
+++ api/models/position.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 const db = require('../db')
-
+const Unit = require(Unit)
 
 //example: Tightend - TE
 module.exports = db.connection.define('Position',
@@ -18,10 +18,6 @@ module.exports = db.connection.define('Position',
 		positionAbbreviation: {
 			field: 'positionAbbreviation',
 			type: Sequelize.STRING
-		},
-		positionImportance: {
-			field: 'positionImportance',
-			type: Sequelize.INTEGER
 		}
 	},
 	{
@@ -29,31 +25,4 @@ module.exports = db.connection.define('Position',
 	}
 )
 
-
-positions = [
-	{positionName: 'Quarterback', positionAbbreviation: 'QB', positionImportance: 100},
-	{positionName: 'Offensive Guard', positionAbbreviation: 'OLG', positionImportance: 80},
-	{positionName: 'Offensive Tackle', positionAbbreviation: 'OLT', positionImportance: 90},
-	{positionName: 'Center', positionAbbreviation: 'OLC', positionImportance: 85},
-	{positionName: 'Wide Receiver', positionAbbreviation: 'WR', positionImportance: 75},
-	{positionName: 'Running Back', positionAbbreviation: 'RB', positionImportance: 70},
-	{positionName: 'Tight End', positionAbbreviation: 'TE', positionImportance: 60},
-	{positionName: 'Fullback', positionAbbreviation: 'FB', positionImportance: 50},
-	{positionName: 'Inside Linebacker', positionAbbreviation: 'ILB', positionImportance: 80},
-	{positionName: 'Edge', positionAbbreviation: 'Edge', positionImportance: 90},
-	{positionName: 'Defensive Line', positionAbbreviation: 'DL', positionImportance: 75},
-	{positionName: 'Nose Tackle', positionAbbreviation: 'NT', positionImportance: 60},
-	{positionName: 'Cornerback', positionAbbreviation: 'CB', positionImportance: 90},
-	{positionName: 'Safety', positionAbbreviation: 'S', positionImportance: 80},
-	{positionName: 'Punter', positionAbbreviation: 'P', positionImportance: 20},
-	{positionName: 'Kicker', positionAbbreviation: 'K', positionImportance: 20},
-	{positionName: 'Long Snapper', positionAbbreviation: 'LS', positionImportance: 10}
-];
-
-
-Position.bulkCreate(positions,{validate:true}).then(()=>{
-	console.log('positions created')
-});
-
-module.exports = Position;
-
+Position.belongsTo(Unit)
